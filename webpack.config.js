d94e7ea491b8b72b086e318aa6d3074fe735cd29
Liudmila Fedorova <ritsumei@yandex.com>
2023-06-12 17:16:41 +0200

    feat: create colors-type page

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const PugPlugin = require('pug-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+  },
+
+  entry: {
+    'index': './src/index.pug',
+    'colors-type': './src/colors-type.pug'
+  },
+
+  mode: 'development',
+
+  plugins: [
+    new PugPlugin({
+      pretty: true, // formatting HTML, useful for development mode
+      js: {
+        // output filename of extracted JS file from source script
+        filename: './js/[name].[contenthash:8].js',
+      },
+      css: {
+        // output filename of extracted CSS file from source style
+        filename: './css/[name].[contenthash:8].css',
+      },
+    }),
+  ],
+
+  module: {
+    rules: [{
+      test: /\.pug$/,
+      loader: PugPlugin.loader, // Pug loader
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    },
+    {
+      test: /\.(css|sass|scss)$/,
+      use: ['css-loader', 'postcss-loader', 'sass-loader'],
+    },
+    {
+      test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+      type: 'asset/inline',
+    },
+    {
+      test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+      type: 'asset/resource',
+    },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    watchFiles: {
+      paths: ['src/**/*.*'],
+      options: {
+        usePolling: true,
+      },
+    },
+  },
+};
